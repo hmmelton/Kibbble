81bc106ad64a1b82a3a091814937a874fbae328c
Harrison Melton <hmmelton@comcast.net>
2017-03-18 19:21:44 -0700

    changed launcher icon, added fragment classes & layouts, Myo util file

---
+++ app/src/main/java/com/hmmelton/kibbble/utils/MyoUtil.java
@@ -0,0 +1,32 @@
+package com.hmmelton.kibbble.utils;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.thalmic.myo.DeviceListener;
+import com.thalmic.myo.Hub;
+import com.thalmic.myo.scanner.ScanActivity;
+
+/**
+ * Created by harrisonmelton on 3/18/17.
+ * This is a utility class for Thalmic Myo integration.
+ */
+
+public class MyoUtil {
+    public static void setUpMyo(Context context, DeviceListener listener) {
+        // Get instance of hub, if there is one
+        Hub hub = Hub.getInstance();
+        if (!hub.init(context)) {
+            return;
+        }
+
+        // Start Myo scan
+        Intent intent = new Intent(context, ScanActivity.class);
+        context.startActivity(intent);
+
+        // Set locking policy
+        Hub.getInstance().setLockingPolicy(Hub.LockingPolicy.STANDARD);
+
+        Hub.getInstance().addListener(listener); // Add listener
+    }
+}
