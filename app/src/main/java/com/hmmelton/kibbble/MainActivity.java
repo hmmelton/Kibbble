2c530c5c86da98c93f4c2b0fd6c8eb78c403cc77
Harrison Melton <hmmelton@comcast.net>
2017-03-18 21:04:21 -0700

    viewpager shit

--- app/src/main/java/com/hmmelton/kibbble/MainActivity.java
+++ app/src/main/java/com/hmmelton/kibbble/MainActivity.java
@@ -1,11 +1,23 @@
 package com.hmmelton.kibbble;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.hmmelton.kibbble.fragments.MainFragment;
+import com.hmmelton.kibbble.fragments.ProfileFragment;
+import com.hmmelton.kibbble.fragments.SavedFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
+    @BindView(R.id.pager)
+    ViewPager mPager;
+
+    // OnClick methods
+    @OnClick(R.id.toolbar_title)
+    void onTitleClick() {
+        mPager.setCurrentItem(1, true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +47,43 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         // Remove default title
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        setUpViewPager();
+
+        // Set pager to middle item
+        mPager.setCurrentItem(1);
+    }
+
+    private void setUpViewPager() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        // Add Fragments
+        adapter.addFragment(ProfileFragment.newInstance());
+        adapter.addFragment(MainFragment.newInstance());
+        adapter.addFragment(SavedFragment.newInstance());
+        mPager.setAdapter(adapter);
+    }
+
+    // Adapter class for view pager tabs
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+
+        ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        void addFragment(Fragment fragment) {
+            mFragmentList.add(fragment);
+        }
     }
 }
