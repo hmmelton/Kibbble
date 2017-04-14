e0c4f7522631d7c40d84c31fce7e53076e5f1030
Harrison Melton <hmmelton@comcast.net>
2017-04-13 23:00:44 -0700

    added empty screen message

--- app/src/main/java/com/hmmelton/kibbble/fragments/MainFragment.java
+++ app/src/main/java/com/hmmelton/kibbble/fragments/MainFragment.java
@@ -8,14 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.hmmelton.kibbble.R;
-import com.hmmelton.kibbble.models.Profile;
-import com.hmmelton.kibbble.utils.JsonUtil;
-import com.hmmelton.kibbble.views.Card;
 import com.mindorks.placeholderview.SwipeDecor;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,8 +20,6 @@ import butterknife.ButterKnife;
  */
 
 public class MainFragment extends Fragment {
-
-    private List<Profile> mProfiles;
 
     @SuppressWarnings("unused")
     private final String TAG = getClass().getSimpleName();
@@ -41,9 +33,6 @@ public class MainFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         ButterKnife.bind(this, rootView);
 
-        mProfiles = new ArrayList<>();
-
-
         // Create swipe view
         mSwipeView.getBuilder()
                 .setDisplayViewCount(3)
@@ -53,11 +42,7 @@ public class MainFragment extends Fragment {
                         .setSwipeInMsgLayoutId(R.layout.swipe_in_msg_view)
                         .setSwipeOutMsgLayoutId(R.layout.swipe_out_msg_view));
 
-        // Convert each JSON object to a Profile object, and display in swipe view
-        for(Profile profile : JsonUtil.loadProfiles(getContext())) {
-            mSwipeView.addView(new Card(getActivity(), profile, mSwipeView));
-            mProfiles.add(profile);
-        }
+        // TODO: Add pets to view
 
         return rootView;
     }
