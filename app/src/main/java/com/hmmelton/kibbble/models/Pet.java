39f1fdeb5a18dc5d2366a72d03fe20f1763f4128
Harrison Melton <hmmelton@comcast.net>
2017-04-15 15:57:34 -0700

    finished Pet model

--- app/src/main/java/com/hmmelton/kibbble/models/Pet.java
+++ app/src/main/java/com/hmmelton/kibbble/models/Pet.java
@@ -2,7 +2,74 @@ package com.hmmelton.kibbble.models;
 
 /**
  * Created by harrison on 4/15/17.
+ * This is a model class representing a pet available for adoption.
  */
 
 public class Pet {
+
+    private String mId;
+    private String mName;
+    private String mBreed;
+    private int mAge;
+    private String mGender;
+    private String mSize;
+    private String mColor;
+    private boolean mSpayedNeutered;
+    private boolean mHousetrained;
+    private String mBio;
+    private String[] mImages;
+    private String mShelterId;
+
+    // Empty constructor for Firebase
+    public Pet() {}
+
+    /* Getter methods */
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getBreed() {
+        return mBreed;
+    }
+
+    public int getAge() {
+        return mAge;
+    }
+
+    public String getGender() {
+        return mGender;
+    }
+
+    public String getSize() {
+        return mSize;
+    }
+
+    public String getColor() {
+        return mColor;
+    }
+
+    public boolean isSpayedNeutered() {
+        return mSpayedNeutered;
+    }
+
+    public boolean isHousetrained() {
+        return mHousetrained;
+    }
+
+    public String getBio() {
+        return mBio;
+    }
+
+    public String[] getImages() {
+        return mImages;
+    }
+
+    public String getShelterId() {
+        return mShelterId;
+    }
 }
