d1369cfabcf2cae237f5a22ac68860e4fe4d2ee1
Harrison Melton <hmmelton@comcast.net>
2017-04-16 21:27:20 -0700

    locked profiles into portrait

---
+++ app/src/main/java/com/hmmelton/kibbble/views/SquareCardView.java
@@ -0,0 +1,10 @@
+package com.hmmelton.kibbble.views;
+
+import android.support.v7.widget.CardView;
+
+/**
+ * Created by harrison on 4/16/17.
+ */
+
+public class SquareCardView extends CardView {
+}
